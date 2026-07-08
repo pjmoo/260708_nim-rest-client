23dcc5833a460a8718bd78fd93d46b404cc85da9
pjmoo <pjmoooo@gmail.com>
2026-07-08 14:27:31 +0900

    Initial commit

---
+++ src/main/java/org/example/nimrestclient/NimRestClientApplication.java
@@ -0,0 +1,13 @@
+package org.example.nimrestclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NimRestClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NimRestClientApplication.class, args);
+    }
+
+}
