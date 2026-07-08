e19fc7b49bf561db8a8fbbe06b1046fba9a8b0a5
pjmoo <pjmoooo@gmail.com>
2026-07-08 15:38:41 +0900

    feat: RestClient 기본 설정 추가

--- src/main/java/org/example/nimrestclient/util/NimUtil.java
+++ src/main/java/org/example/nimrestclient/util/NimUtil.java
@@ -1,13 +1,21 @@
 package org.example.nimrestclient.util;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
 
 @Component
 public class NimUtil {
     private final String nimApiKey = System.getenv("NIM_API_KEY");
+    private final String nimApiURL = "https://integrate.api.nvidia.com/v1/chat/completions";
+    private final RestClient restClient;
 
-    NimUtil() {
+    protected NimUtil() {
         System.out.println("nimApiKey: " + nimApiKey.substring(0, 10) + "...");
+        restClient = RestClient.builder()
+                .baseUrl(nimApiURL)
+                .defaultHeader("Authorization", "Bearer %s".formatted(nimApiKey))
+                .defaultHeader("Content-Type", "application/json")
+                .build();
     }
 
     public String useNim(String prompt) {
