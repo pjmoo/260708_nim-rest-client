33798a3bb6dc98e05b0271f2ebec01ae9ed4b8e3
pjmoo <pjmoooo@gmail.com>
2026-07-08 15:28:35 +0900

    feat: NIM API Key 환경변수 설정 추가

--- src/main/java/org/example/nimrestclient/util/NimUtil.java
+++ src/main/java/org/example/nimrestclient/util/NimUtil.java
@@ -4,6 +4,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class NimUtil {
+    private final String nimApiKey = System.getenv("NIM_API_KEY");
+
+    NimUtil() {
+        System.out.println("nimApiKey: " + nimApiKey.substring(0, 10) + "...");
+    }
+
     public String useNim(String prompt) {
         return "%s라니 멋지군요!".formatted(prompt);
     }
